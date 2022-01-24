f557ce1c960e33fde02d1916d3cb6ef86080c9f9
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-01-24 11:25:42 +0100

    :sparkles: add show create post button

--- src/components/CreatePostView.tsx
+++ src/components/CreatePostView.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import AppwriteService from "../database/appwriteService";
 import "./CreatePostView.css";
 
 export default function CreatePostView(props: {
   appwriteService: AppwriteService;
 }) {
-  const [message, setMessage] = React.useState("");
+  const [message, setMessage] = useState("");
+  const [active, setActive] = useState<Boolean>(false);
 
-  async function onSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
+  async function onSubmitHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     try {
       await props.appwriteService.createPost(message);
@@ -18,16 +19,28 @@ export default function CreatePostView(props: {
   }
 
   return (
-    <form className="CreatePostView" onSubmit={onSubmitHandler}>
-      <textarea
-        className="CreatePostViewMessageInput"
-        value={message}
-        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
-          setMessage(event.target.value.slice(0, 8192))
-        }
-        required
-      ></textarea>
-      <button type="submit">Create Post</button>
-    </form>
+    <>
+      <button
+        className="ShowCreatePostButton"
+        onClick={() => setActive(!active)}
+      >
+        {active ? "Hide" : "Show"} Create Post
+      </button>
+      {active ? (
+        <form className="CreatePostView" onSubmit={onSubmitHandler}>
+          <textarea
+            className="CreatePostViewMessageInput"
+            value={message}
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setMessage(event.target.value.slice(0, 8192))
+            }
+            required
+          ></textarea>
+          <button type="submit">Create Post</button>
+        </form>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
