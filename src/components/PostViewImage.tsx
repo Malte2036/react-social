461d575a5fafd9e1585dca5abe64cec347e615fe
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-01-27 13:00:58 +0100

    :lipstick: add margin top to PostViewImage

--- src/components/PostViewImage.tsx
+++ src/components/PostViewImage.tsx
@@ -1,3 +1,9 @@
 export default function PostViewImage(props: { image: string }) {
-  return <img className="w-full rounded-lg border-1 border-gray-200 dark:border-gray-900" src={props.image} alt="" />;
+  return (
+    <img
+      className="w-full mt-3 rounded-lg border-1 border-gray-200 dark:border-gray-900"
+      src={props.image}
+      alt=""
+    />
+  );
 }
