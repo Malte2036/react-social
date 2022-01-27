637856e6b0bc08f8db0b38c47b5203a376fadbd0
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-01-27 11:45:55 +0100

    :lipstick: improve WelcomePage design

--- src/routes/welcome/WelcomePage.tsx
+++ src/routes/welcome/WelcomePage.tsx
@@ -31,42 +31,38 @@ export default function WelcomePage(props: {
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        {welcomeState != null ? (
-          <>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <div className="bg-white p-10 m-4 rounded-lg border-2">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            {welcomeState === welcomeStateType.register
+              ? "Register "
+              : "Sign in to "}
+            your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Or{" "}
+            <span
+              onClick={() =>
+                setWelcomeState(
+                  welcomeState === welcomeStateType.register
+                    ? welcomeStateType.login
+                    : welcomeStateType.register
+                )
+              }
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
               {welcomeState === welcomeStateType.register
-                ? "Register "
-                : "Sign in to "}{" "}
-              your account
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{" "}
-              <span
-                onClick={() =>
-                  setWelcomeState(
-                    welcomeState === welcomeStateType.register
-                      ? welcomeStateType.login
-                      : welcomeStateType.register
-                  )
-                }
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                {welcomeState === welcomeStateType.register
-                  ? "login"
-                  : "register"}
-              </span>
-            </p>
-            {welcomeState === welcomeStateType.register ? (
-              <RegisterForm
-                appwriteService={props.appwriteService}
-              ></RegisterForm>
-            ) : (
-              <LoginForm appwriteService={props.appwriteService}></LoginForm>
-            )}
-          </>
-        ) : (
-          <></>
-        )}
+                ? "login"
+                : "register"}
+            </span>
+          </p>
+          {welcomeState === welcomeStateType.register ? (
+            <RegisterForm
+              appwriteService={props.appwriteService}
+            ></RegisterForm>
+          ) : (
+            <LoginForm appwriteService={props.appwriteService}></LoginForm>
+          )}
+        </div>
       </div>
     </div>
   );
