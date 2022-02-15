bd7441b55cfc1ef9734cfef3832b1d503d1f57bd
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-15 23:08:37 +0100

    :construction: add settings page

---
+++ pages/settings/index.tsx
@@ -0,0 +1,17 @@
+import ToogleSwitch from "../../components/form/ToogleSwitch";
+
+export default function SettingsPage(props: {}) {
+  return (
+    <div>
+      <h1 className="pt-6 text-center text-5xl font-extrabold ">Settings</h1>
+      <br />
+      <div className="flex justify-center">
+        <h2>Darkmode:</h2>
+        <ToogleSwitch
+          toogle={props.darkmode}
+          setToogle={() => props.setDarkmode(!props.darkmode)}
+        ></ToogleSwitch>
+      </div>
+    </div>
+  );
+}
