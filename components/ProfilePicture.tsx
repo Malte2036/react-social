63db836887a187d2eb5dbea6a1b4091461d3f3d7
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-04-04 17:04:41 +0200

    :sparkles: add default profile picture

--- components/ProfilePicture.tsx
+++ components/ProfilePicture.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import BackendService from "../lib/database/backendService";
 import { MyFile } from "../lib/database/data/myFile";
+import defaultProfilePicture from "../public/default_profile_picture.png";
 
 export default function ProfilePicture(props: {
   backendService: BackendService;
@@ -14,25 +15,30 @@ export default function ProfilePicture(props: {
     props.imageId === null ? null : undefined
   );
   useEffect(() => {
-    props.backendService
-      .getFileById(props.imageId!, cookie.bearerToken)
-      .then((myFile) => setImage(myFile));
+    if (props.imageId != null) {
+      props.backendService
+        .getFileById(props.imageId!, cookie.bearerToken)
+        .then((myFile) => setImage(myFile));
+    }
   }, [cookie.bearerToken, props.backendService, props.imageId]);
+
+  let imageComponent: JSX.Element | undefined;
+  if (image !== undefined) {
+    imageComponent = (
+      <Image
+        className="rounded-full object-cover border-2 border-gray-200 dark:border-gray-900"
+        src={image === null ? defaultProfilePicture : image.data}
+        alt=""
+        layout="responsive"
+        width="64px"
+        height="64px"
+      />
+    );
+  }
 
   return (
     <div className={`h-${props.size ?? 8} w-${props.size ?? 8}`}>
-      {image ? (
-        <Image
-          className="rounded-full object-cover border-2 border-gray-200 dark:border-gray-900"
-          src={image.data}
-          alt=""
-          layout="responsive"
-          width="64px"
-          height="64px"
-        />
-      ) : (
-        <></>
-      )}
+      {imageComponent ?? <></>}
     </div>
   );
 }
